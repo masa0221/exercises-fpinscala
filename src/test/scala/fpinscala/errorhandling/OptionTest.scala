005c3e86b02504aef8c5a315be86ac8a02be7ca3
Masashi Tsuru <0221masa@gmail.com>
2022-01-15 12:36:21 +0900

    Impl map method in Option

--- src/test/scala/fpinscala/errorhandling/OptionTest.scala
+++ src/test/scala/fpinscala/errorhandling/OptionTest.scala
@@ -5,9 +5,12 @@ import org.scalatest.freespec.AnyFreeSpecLike
 import org.scalatest.matchers.should.Matchers
 
 class OptionTest extends AnyFreeSpecLike with Matchers:
-  "no implements" - {
-    "test something" in {
-      1 `shouldBe` 1
+  "map" - {
+    "Noneの場合はNone" in {
+      None.map(a => a) `shouldBe` None
+    }
+    "Someの場合は関数が適応される" in {
+      Some(10).map(_ * 10) `shouldBe` Some(100)
     }
   }
 
