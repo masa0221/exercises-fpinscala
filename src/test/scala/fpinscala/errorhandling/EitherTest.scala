7aefdc45a4982514e644056658835adb11f487f2
Masashi Tsuru <0221masa@gmail.com>
2022-01-21 14:22:11 +0900

    Add test cases on Either

--- src/test/scala/fpinscala/errorhandling/EitherTest.scala
+++ src/test/scala/fpinscala/errorhandling/EitherTest.scala
@@ -1,13 +1,46 @@
 package fpinscala.errorhandling
 
-import fpinscala.errorhandling.Either
+import fpinscala.errorhandling.Either.*
 import org.scalatest.freespec.AnyFreeSpecLike
 import org.scalatest.matchers.should.Matchers
 
 class EitherTest extends AnyFreeSpecLike with Matchers:
-  "no implements" - {
-    "test something" in {
-      1 `shouldBe` 1
+  "map" - {
+    "Leftの場合はmapは関数が適用されないこと" in {
+      Left(10).map(_ => "type A,B not found") `shouldBe` Left(10)
+    }
+    "Rightの場合は関数が適用されること" in {
+      Right(10).map(_ * 10) `shouldBe` Right(100)
+    }
+  }
+
+  "flatMap" - {
+    "Leftの場合は何も処理されないこと" in {
+      Left("error").flatMap(a => Right("right!")) `shouldBe` Left("error")
+    }
+    "Rightの場合は関数が適用されること" in {
+      Right(2).flatMap(a => Right(a * 100)) `shouldBe` Right(200)
+    }
+  }
+
+  "orElse" - {
+    "Leftの場合は引数の値が取得できること" in {
+      Left("").orElse(Left("error")) `shouldBe` Left("error")
+    }
+    "Rightの場合はRightが持っている値が取得できること" in {
+      Right(4).orElse(Left(5)) `shouldBe` Right(4)
+    }
+  }
+
+  "map2" - {
+    "Leftの場合はLeftを返すこと" in {
+      Left("error").map2(Right(1))((a, b) => "failed") `shouldBe` Left("error")
+    }
+    "元の値がRightだとしてしてもMap2の内容がLeftの場合はLeftを返すこと" in {
+      Right(1).map2(Left("error"))((a, b) => "failed") `shouldBe` Left("error")
+    }
+    "両方Rightの場合は関数が適用された結果のRightを返すこと" in {
+      Right(1).map2(Right(2))((a, b) => a + b) `shouldBe` Right(3)
     }
   }
 
