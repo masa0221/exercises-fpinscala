c74e42d4fb8b3fc445c96c5b8e34368c2510f5c1
Masashi Tsuru <0221masa@gmail.com>
2022-01-30 09:56:39 +0900

    Add test file for LazyList

---
+++ src/test/scala/fpinscala/laziness/LazyListTest.scala
@@ -0,0 +1,14 @@
+package fpinscala.laziness
+
+import fpinscala.laziness.LazyList.*
+import org.scalatest.freespec.AnyFreeSpecLike
+import org.scalatest.matchers.should.Matchers
+
+class LazyListTest extends AnyFreeSpecLike with Matchers:
+  "unimplemnts" - {
+    "test case" in {
+      true `shouldBe` true
+    }
+  }
+
+end LazyListTest
