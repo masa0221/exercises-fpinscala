91073337d4b889f96cf48ccf0dd6496ea8705420
Masashi Tsuru <0221masa@gmail.com>
2022-01-30 10:41:37 +0900

    Impl toList method in LazyList

--- src/test/scala/fpinscala/laziness/LazyListTest.scala
+++ src/test/scala/fpinscala/laziness/LazyListTest.scala
@@ -7,7 +7,7 @@ import org.scalatest.matchers.should.Matchers
 class LazyListTest extends AnyFreeSpecLike with Matchers:
   "toList" - {
     "値が展開されたListを取得できること" in {
-      LazyList(1,2,3,4,5).toList `shouldBe` List(1,2,3,4,5)
+      LazyList(1, 2, 3, 4, 5).toList `shouldBe` List(1, 2, 3, 4, 5)
     }
   }
 
