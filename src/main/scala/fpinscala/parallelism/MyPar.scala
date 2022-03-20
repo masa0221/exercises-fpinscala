8befc1b32887f9d2631753c0aba489075ddf43dd
Masashi Tsuru <0221masa@gmail.com>
2022-03-20 14:54:45 +0900

    Impl sum method that divids the list in half and calcurate

--- src/main/scala/fpinscala/parallelism/MyPar.scala
+++ src/main/scala/fpinscala/parallelism/MyPar.scala
@@ -1,6 +1,18 @@
 package fpinscala.parallelism
 
 object MyPar:
+  // def sum(ints: Seq[Int]): Int =
+  //   ints.foldLeft(0)((a, b) => a + b)
+
+  // IndexedSeq: Vector などのスーパークラス
+  def sum(ints: IndexedSeq[Int]): Int =
+    if (ints.size <= 1)
+      ints.headOption.getOrElse(0)
+    else
+      // 半部に分けて計算させる
+      val (l, r) = ints.splitAt(ints.length / 2)
+      sum(l) + sum(r)
+
 end MyPar
 
 object MyExamples:
