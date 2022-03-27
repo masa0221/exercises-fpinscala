0cc93cb6ce6c9ef885bba1b8ded505c272013b88
Masashi Tsuru <0221masa@gmail.com>
2022-03-27 13:38:15 +0900

    Modify MyPar interface(unfinished)

--- src/main/scala/fpinscala/parallelism/MyPar.scala
+++ src/main/scala/fpinscala/parallelism/MyPar.scala
@@ -1,21 +1,39 @@
 package fpinscala.parallelism
 
+import java.util.concurrent.ExecutorService
+import scala.concurrent.Future
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.Callable
+
 case class MyPar[A](value: () => A)
 
 object MyPar:
-  def unit[A](a: A): MyPar[A] = MyPar(() => a)
+  type MyPar[A] = ExecutorService => UnitFuture[A]
+
+  case class UnitFuture[A](value: A) extends Future[A] {
+    def isDone = true
+    def get(timeout: Long, units: TimeUnit) = value
+    def isCaancelled = false
+    def cancel(evenIfRunning: Boolean): Boolean = false
+  }
+
+  def unit[A](a: A): MyPar[A] = (es: ExecutorService) => UnitFuture(a)
 
   def lazyUnit[A](a: => A): MyPar[A] = fork(unit(a))
 
   def map2[A, B, C](a: MyPar[A], b: MyPar[B])(f: (A, B) => C): MyPar[C] =
-    lazyUnit(f(run(a), run(b)))
+    (es: ExecutorService) =>
+      val af = a(es)
+      val bf = b(es)
+      UnitFuture(f(af.get(1L, TimeUnit.SECONDS), bf.get(1L, TimeUnit.SECONDS)))
+      // lazyUnit(f(run(a), run(b)))
 
   def fork[A](a: => MyPar[A]): MyPar[A] =
-    // TODO: forkの実装がわからん
-    lazy val value = a
-    MyPar(() => run(value))
+    es => es.submit(new Callable[A] {
+      def call = a(es).get(1, TimeUnit.SECONDS)
+    })
 
-  def run[A](a: MyPar[A]): A = a.value()
+  def run[A](s: ExecutorService)(a: MyPar[A]): Future[A] = a(s)
 
 end MyPar
 
