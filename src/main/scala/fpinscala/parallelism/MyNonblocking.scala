65e75d523c74dd2d8bfe621d622c5e4c8df12e76
Masashi Tsuru <0221masa@gmail.com>
2022-04-10 15:00:27 +0900

    Impl map2 method in MyNonblocking

--- src/main/scala/fpinscala/parallelism/MyNonblocking.scala
+++ src/main/scala/fpinscala/parallelism/MyNonblocking.scala
@@ -20,8 +20,8 @@ object MyNonblocking:
     latch.await
     ref.get
 
-  def unit[A](a: A): MyPar[A] = 
-    es => 
+  def unit[A](a: A): MyPar[A] =
+    es =>
       new Future[A]:
         def apply(cb: A => Unit): Unit = cb(a)
 
@@ -37,7 +37,26 @@ object MyNonblocking:
     es.submit(
       new Callable[Unit]:
         def call = r
-      )
+    )
+
+  def map2[A, B, C](p: MyPar[A], p2: MyPar[B])(f: (A, B) => C): MyPar[C] =
+    es =>
+      new Future[C]:
+        def apply(cb: C => Unit): Unit =
+          var ar: Option[A] = None
+          var br: Option[B] = None
+          val combinater = Actor[Either[A, B]](es) {
+            case Left(a) =>
+              br match
+                case None    => ar = Some(a)
+                case Some(b) => eval(es)(cb(f(a, b)))
+            case Right(b) =>
+              ar match
+                case None    => br = Some(b)
+                case Some(a) => eval(es)(cb(f(a, b)))
+          }
+          p(es)(a => combinater ! Left(a))
+          p2(es)(b => combinater ! Right(b))
 
   object MyPar:
   end MyPar
