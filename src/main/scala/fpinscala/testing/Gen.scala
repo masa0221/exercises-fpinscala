6d4c38bcfcd27b5243791220fa51abe3f6a1d067
Masashi Tsuru <0221masa@gmail.com>
2022-04-24 14:52:27 +0900

    Impl && method in Gen

--- src/main/scala/fpinscala/testing/Gen.scala
+++ src/main/scala/fpinscala/testing/Gen.scala
@@ -1,7 +1,17 @@
 package fpinscala.testing
 
 trait Prop:
-  def &&(p: Prop): Prop
+  // self type annotation
+  // @see http://www.ne.jp/asahi/hishidama/home/tech/scala/class.html#h_class.this
+  // thisじゃだめなのか、ええやんthisで、って思っちゃう
+  self =>
+
+  def &&(that: Prop): Prop =
+    new Prop:
+      // that なら thisでええやないかと思っちゃうんだけど違うんか・・・郷にいればなんとやらか。
+      def check = self.check && that.check
+
+  def check: Boolean
 
 object Prop:
   def forAll[A](a: Gen[A])(f: A => Boolean): Prop = ???
