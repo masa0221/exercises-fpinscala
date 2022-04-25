c3475806b3592ed8fe5e46e9cc7b0cc02902b55b
Masashi Tsuru <0221masa@gmail.com>
2022-04-25 10:55:51 +0900

    Modify check method so that return Either

--- src/main/scala/fpinscala/testing/Gen.scala
+++ src/main/scala/fpinscala/testing/Gen.scala
@@ -6,16 +6,24 @@ trait Prop:
   // thisじゃだめなのか、ええやんthisで、って思っちゃう
   self =>
 
-  def &&(that: Prop): Prop =
-    new Prop:
-      // that なら thisでええやないかと思っちゃうんだけど違うんか・・・郷にいればなんとやらか。
-      def check = self.check && that.check
+  // checkがEitherに変わったため一旦未実装に戻す
+  def &&(that: Prop): Prop = ???
+  // new Prop:
+  //   // that なら thisでええやないかと思っちゃうんだけど違うんか・・・郷にいればなんとやらか。
+  //   def check = self.check && that.check
 
-  def check: Boolean
+  import fpinscala.testing.Prop.*
+  // 失敗の型を定義していないのは、
+  // 失敗したケースでさらに計算を行う必要がないから
+  // (失敗した情報の出力のみ行えば良いので)
+  def check: Either[(FailedCase, SuccessCount), SuccessCount]
 
 object Prop:
+  // 型エイリアスを設定するとコードが読みやすくなる
+  type SuccessCount = Int
+  type FailedCase = String
+
   def forAll[A](a: Gen[A])(f: A => Boolean): Prop = ???
 
 trait Gen[A]:
   def listOfN[A](n: Int, a: Gen[A]): Gen[List[A]] = ???
-
