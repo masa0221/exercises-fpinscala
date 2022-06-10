f77e1302479ba52886ae59e21497761c4de2976e
Masashi Tsuru <0221masa@gmail.com>
2022-06-10 12:03:53 +0900

    Add string method and or method in Parsers

--- src/main/scala/fpinscala/parsing/Parsers.scala
+++ src/main/scala/fpinscala/parsing/Parsers.scala
@@ -1,6 +1,21 @@
 package fpinscala.parsing
 
 trait Parsers[ParseError, Parser[+_]] {
+  // 成り立つ式
+  // run(char(c))(c.toString) == Right(c)
+  // run(string(s))(s) == Right(c)
+  // run(or(string("hoge"), string("fuga")))("hoge") == Right("hoge")
+  // run(or(string("hoge"), string("fuga")))("fuga") == Right("fuga")
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
+
+  // 任意の一文字を認識
   def char(c: Char): Parser[Char]
+
+  // 任意の文字列を認識
+  def string(s: String): Parser[String]
+
+  // 二つの文字列のどちらかをの文字列を認識
+  // def orString(s1: String, s2: String): Parser[String]
+  // ↓多相にしてみる
+  def or(s1: Parser[String], s2: Parser[String]): Parser[String]
 }
