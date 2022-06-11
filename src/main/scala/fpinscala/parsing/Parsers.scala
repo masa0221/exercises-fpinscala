78d64163a7b7385489b6ed5930a56b112516fc69
Masashi Tsuru <0221masa@gmail.com>
2022-06-11 12:53:21 +0900

    Impl ParserOps class

--- src/main/scala/fpinscala/parsing/Parsers.scala
+++ src/main/scala/fpinscala/parsing/Parsers.scala
@@ -1,6 +1,6 @@
 package fpinscala.parsing
 
-trait Parsers[ParseError, Parser[+_]] {
+trait Parsers[ParseError, Parser[+_]] { self =>
   // 成り立つ式
   // run(char(c))(c.toString) == Right(c)
   // run(string(s))(s) == Right(c)
@@ -12,10 +12,21 @@ trait Parsers[ParseError, Parser[+_]] {
   def char(c: Char): Parser[Char]
 
   // 任意の文字列を認識
-  def string(s: String): Parser[String]
+  // def string(s: String): Parser[String]
+  implicit def string(s: String): Parser[String]
 
   // 二つの文字列のどちらかをの文字列を認識
   // def orString(s1: String, s2: String): Parser[String]
   // ↓多相にしてみる
-  def or(s1: Parser[String], s2: Parser[String]): Parser[String]
+  // def or(s1: Parser[String], s2: Parser[String]): Parser[String]
+  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+
+  implicit def operators[A](p: Parser[A]): ParserOps[A] = ParserOps[A](p)
+  // def string と def asStringParser によって Stringが自動的にParserに昇格される
+  implicit def asStringParser[A](a: A)(implicit
+      f: A => Parser[String]
+  ): ParserOps[String] = ParserOps(f(a))
+  case class ParserOps[A](p: Parser[A]):
+    def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+    def or[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
 }
