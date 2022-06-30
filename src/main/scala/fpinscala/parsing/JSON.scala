d1d36553955f69ef65e10f4ac642020791299cc1
Masashi Tsuru <0221masa@gmail.com>
2022-06-30 13:57:55 +0900

    Add JSON.scala for product method

---
+++ src/main/scala/fpinscala/parsing/JSON.scala
@@ -0,0 +1,17 @@
+package fpinscala.parsing
+
+import language.higherKinds
+
+// TODO: exercise 9.2 がわからなかったので以下を写経する
+// https://github.com/fpinscala/fpinscala/blob/first-edition/answers/src/main/scala/fpinscala/parsing/JSON.scala
+// https://github.com/fpinscala/fpinscala/blob/second-edition/src/main/scala/fpinscala/answers/parsing/JSON.scala
+enum JSON:
+  case JNull
+  case JNumber(get: Double)
+  case JString(get: String)
+  case JBool(get: Boolean)
+  case JArray(get: IndexedSeq[JSON])
+  case JObject(get: Map[String, JSON])
+
+object JSON:
+  def jsonParser[Parser[+_]](P: Parsers[Parser]): Parser[JSON] = ???
