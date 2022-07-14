961a9676cbf55849157aadd75b9036c9d48e9c3e
Masashi Tsuru <0221masa@gmail.com>
2022-07-14 13:45:35 +0900

    Add some methods in jsonParser method

--- src/main/scala/fpinscala/parsing/JSON.scala
+++ src/main/scala/fpinscala/parsing/JSON.scala
@@ -2,8 +2,7 @@ package fpinscala.parsing
 
 import language.higherKinds
 
-// TODO: exercise 9.2 がわからなかったので以下を写経する
-// JSON.scalaは9.2の答えじゃなかった。。(後で使うから残しておこう)
+// TODO: exercise 9.9 がわからなかったので以下を写経する
 // https://github.com/fpinscala/fpinscala/blob/first-edition/answers/src/main/scala/fpinscala/parsing/JSON.scala
 // https://github.com/fpinscala/fpinscala/blob/second-edition/src/main/scala/fpinscala/answers/parsing/JSON.scala
 enum JSON:
@@ -15,9 +14,20 @@ enum JSON:
   case JObject(get: Map[String, JSON])
 
 object JSON:
-  def jsonParser[Err, Parser[+_]](P: Parsers[Err, Parser]): Parser[JSON] = ???
-// import P.*
-// val space = char(' ').many.slice
+  def jsonParser[Err, Parser[+_]](P: Parsers[Err, Parser]): Parser[JSON] =
+    import P.*
+    // [] で囲まれた文字をarrayとする
+    def array = ???
 
-// // token???
-// def token(s: String) = string(s).token
+    // {} で囲まれた文字をobjectとする
+    def obj = ???
+
+    // "key": "value" の形?
+    def keyval = ???
+
+    // 実際の値(literalの略)
+    def lit = ???
+
+    def value: Parser[JSON] = ???
+
+    ???
