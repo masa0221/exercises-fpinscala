2276f0b1e3f9ca8aa54899b5c68c46558b863c98
Masashi Tsuru <0221masa@gmail.com>
2022-08-29 13:33:23 +0900

    Impl op method and zero method

--- src/main/scala/fpinscala/monoids/Monoid.scala
+++ src/main/scala/fpinscala/monoids/Monoid.scala
@@ -1,3 +1,9 @@
 package fpinscala.monoids
 
-trait Monoid[A]
+trait Monoid[A]:
+  def op(a1: A, a2: A): A
+  def zero: A
+
+val stringMonad = new Monoid[String]:
+  def op(a1: String, a2: String): String = a1 + a2
+  def zero: String = ""
---
+++ src/test/scala/fpinscala/monoids/MonoidTest.scala
@@ -0,0 +1,11 @@
+package fpinscala.monoids
+
+import org.scalatest.freespec.AnyFreeSpecLike
+import org.scalatest.matchers.should.Matchers
+
+class MonoidTest extends AnyFreeSpecLike with Matchers:
+  "stringMonad" - {
+    "足し算ができること" in {
+      stringMonad.op("abcd", "efg") should equal("abcdefg")
+    }
+  }
