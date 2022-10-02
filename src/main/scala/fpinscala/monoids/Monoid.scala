a19a4a8b9ead9e444b7f46932eba0418261986d2
Masashi Tsuru <0221masa@gmail.com>
2022-10-02 12:20:06 +0900

    Impl foldLeft in Foldable

--- src/main/scala/fpinscala/monoids/Monoid.scala
+++ src/main/scala/fpinscala/monoids/Monoid.scala
@@ -142,7 +142,9 @@ trait Foldable[F[_]]:
   def foldRight[A, B](as: F[A])(z: B)(f: (A, B) => B): B =
     foldMap(as)(f.curried)(endoMonoid[B])(z)
 
-  def foldLeft[A, B](as: F[A])(z: B)(f: (B, A) => B): B = ???
+  def foldLeft[A, B](as: F[A])(z: B)(f: (B, A) => B): B =
+    foldMap(as)(a => b => f(b, a))(dual(endoMonoid[B]))(z)
+
   def foldMap[A, B](as: F[A])(f: A => B)(mb: Monoid[B]): B = ???
   def concatenate[A](as: F[A])(m: Monoid[A]): A = ???
 
