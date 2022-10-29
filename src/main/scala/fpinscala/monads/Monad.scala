2a5ce74b5dc2be300d8cfd71fa1af4902fa01260
Masashi Tsuru <0221masa@gmail.com>
2022-10-29 14:10:31 +0900

    Add Mon class

--- src/main/scala/fpinscala/monads/Monad.scala
+++ src/main/scala/fpinscala/monads/Monad.scala
@@ -13,3 +13,11 @@ trait Functor[F[_]]:
 
 val listFunctor = new Functor[List]:
   def map[A, B](as: List[A])(f: A => B): List[B] = as map f
+
+trait Mon[F[_]]:
+  def map[A, B](fa: F[A])(f: A => B): F[B]
+
+  def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
+
+  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
+    flatMap(fa)(a => map(fb)(b => f(a, b)))
