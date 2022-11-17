f0c52f2b54fb76476883fcf8f6f139b3cca1cc04
Masashi Tsuru <0221masa@gmail.com>
2022-11-17 13:20:39 +0900

    Impl join method

--- src/main/scala/fpinscala/monads/Monad.scala
+++ src/main/scala/fpinscala/monads/Monad.scala
@@ -59,7 +59,7 @@ trait Monad[F[_]]:
   def compose[A, B, C](f: A => F[B], g: B => F[C]): A => F[C] = a =>
     flatMap(f(a))(g)
 
-  def join[A](mma: F[F[A]]): F[A] = ???
+  def join[A](mma: F[F[A]]): F[A] = flatMap(mma)(ma => ma)
 
 object Monad:
   val genMonad = new Monad[Gen]:
