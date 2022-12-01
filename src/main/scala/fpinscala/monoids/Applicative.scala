54fb6827740ff51ddbf5bd676c851fa66cd9f623
Masashi Tsuru <0221masa@gmail.com>
2022-12-01 13:24:59 +0900

    Impl replaceM

--- src/main/scala/fpinscala/monoids/Applicative.scala
+++ src/main/scala/fpinscala/monoids/Applicative.scala
@@ -1,13 +1,25 @@
+package fpinscala
+package applicative
+
+import monads.Functor
+
 trait Applicative[F[_]] extends Functor[F]:
   // プリミティブコンビネータ
-  def map2[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C]
+  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C]
   def unit[A](a: => A): F[A]
 
   // 派生コンビネータ
-  def map[A,B](fa: F[A])(f: A => B): F[B] = map2(fa, unit(()))((a, _) => f(a))
+  def map[A, B](fa: F[A])(f: A => B): F[B] = map2(fa, unit(()))((a, _) => f(a))
 
-  def traverse[A, B](as: List[A])(f: A => F[B]): F[List[B]] = as.foldRight(unit(List[B]()))((a, fbs) => map2(f(a), fbs)(_ :: _))
+  def traverse[A, B](as: List[A])(f: A => F[B]): F[List[B]] =
+    as.foldRight(unit(List[B]()))((a, fbs) => map2(f(a), fbs)(_ :: _))
 
   def sequence[A](fas: List[F[A]]): F[List[A]] = traverse(fas)(fa => fa)
-  def replicateM[A](n: Int, fa: F[A]): F[List[A]]
-  def product[A,B](fa: F[A], fb: F[A]): F[(A,B)] = map2(fa, fb)((a, b) => (a, b))
+
+  def replicateM[A](n: Int, fa: F[A]): F[List[A]] =
+    List
+      .fill(n)(fa)
+      .foldRight(unit(List.empty[A]))((a, b) => map2(a, b)(_ :: _))
+
+  def product[A, B](fa: F[A], fb: F[A]): F[(A, B)] =
+    map2(fa, fb)((a, b) => (a, b))
