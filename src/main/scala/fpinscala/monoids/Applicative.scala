f415e924907a974de73969395c6d5ee76fa312c0
Masashi Tsuru <0221masa@gmail.com>
2022-12-02 13:06:19 +0900

    Fix traverse and replicateM

--- src/main/scala/fpinscala/monoids/Applicative.scala
+++ src/main/scala/fpinscala/monoids/Applicative.scala
@@ -12,14 +12,12 @@ trait Applicative[F[_]] extends Functor[F]:
   def map[A, B](fa: F[A])(f: A => B): F[B] = map2(fa, unit(()))((a, _) => f(a))
 
   def traverse[A, B](as: List[A])(f: A => F[B]): F[List[B]] =
-    as.foldRight(unit(List[B]()))((a, fbs) => map2(f(a), fbs)(_ :: _))
+    as.foldRight(unit(List[B]()))((a, acc) => f(a).map2(acc)(_ :: _))
 
   def sequence[A](fas: List[F[A]]): F[List[A]] = traverse(fas)(fa => fa)
 
   def replicateM[A](n: Int, fa: F[A]): F[List[A]] =
-    List
-      .fill(n)(fa)
-      .foldRight(unit(List.empty[A]))((a, b) => map2(a, b)(_ :: _))
+    sequence(List.fill(n)(fa))
 
   def product[A, B](fa: F[A], fb: F[A]): F[(A, B)] =
     map2(fa, fb)((a, b) => (a, b))
