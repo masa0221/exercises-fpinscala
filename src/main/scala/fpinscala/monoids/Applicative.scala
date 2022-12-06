75173069e26e99b75180405b3e59cd88e927316e
Masashi Tsuru <0221masa@gmail.com>
2022-12-06 19:20:31 +0900

    Fix so that it can be compile

--- src/main/scala/fpinscala/monoids/Applicative.scala
+++ src/main/scala/fpinscala/monoids/Applicative.scala
@@ -8,20 +8,23 @@ trait Applicative[F[_]] extends Functor[F]:
   def apply[A, B](fab: F[A => B])(fa: F[A]): F[B]
   def unit[A](a: => A): F[A]
 
-  // 派生コンビネータ
-  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
-    apply(map(fa)(f.curried), fb)
+  // map2をプリミティブにした場合
+  // def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C]
+  // def apply[A, B](fab: F[A => B])(fa: F[A]): F[B] = map2(fab, fa)(_(_))
 
-  // def map[A, B](fa: F[A])(f: A => B): F[B] = map2(fa, unit(()))((a, _) => f(a))
+  // 派生コンビネータ
+  // def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
+  //   apply(map(fa)(f.curried), fb)
+
   def map[A, B](fa: F[A])(f: A => B): F[B] = apply(unit(f))(fa)
 
-  def traverse[A, B](as: List[A])(f: A => F[B]): F[List[B]] =
-    as.foldRight(unit(List[B]()))((a, acc) => f(a).map2(acc)(_ :: _))
+  // def traverse[A, B](as: List[A])(f: A => F[B]): F[List[B]] =
+  //   as.foldRight(unit(List[B]()))((a, acc) => f(a).map2(acc)(_ :: _))
 
-  def sequence[A](fas: List[F[A]]): F[List[A]] = traverse(fas)(fa => fa)
+  // def sequence[A](fas: List[F[A]]): F[List[A]] = traverse(fas)(fa => fa)
 
-  def replicateM[A](n: Int, fa: F[A]): F[List[A]] =
-    sequence(List.fill(n)(fa))
+  // def replicateM[A](n: Int, fa: F[A]): F[List[A]] =
+  //   sequence(List.fill(n)(fa))
 
-  def product[A, B](fa: F[A], fb: F[A]): F[(A, B)] =
-    map2(fa, fb)((a, b) => (a, b))
+  // def product[A, B](fa: F[A], fb: F[A]): F[(A, B)] =
+  //   map2(fa, fb)((a, b) => (a, b))
