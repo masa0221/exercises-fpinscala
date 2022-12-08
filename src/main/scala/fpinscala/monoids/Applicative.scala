9257153da06b9c6ffe1806bfdcd0a5698cd5421c
Masashi Tsuru <0221masa@gmail.com>
2022-12-08 13:34:13 +0900

    Add map3 and map4

--- src/main/scala/fpinscala/monoids/Applicative.scala
+++ src/main/scala/fpinscala/monoids/Applicative.scala
@@ -13,10 +13,14 @@ trait Applicative[F[_]] extends Functor[F]:
   // def apply[A, B](fab: F[A => B])(fa: F[A]): F[B] = map2(fab, fa)(_(_))
 
   // 派生コンビネータ
+  def map[A, B](fa: F[A])(f: A => B): F[B] = apply(unit(f))(fa)
+
   def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
     apply(map(fa)(f.curried))(fb)
 
-  def map[A, B](fa: F[A])(f: A => B): F[B] = apply(unit(f))(fa)
+  def map3[A, B, C, D](fa: F[A], fb: F[B], fc: F[C])(f: (A, B, C) => D): F[D] = ???
+
+  def map4[A, B, C, D, E](fa: F[A], fb: F[B], fc: F[C], fd: F[D])(f: (A, B, C, D) => E): F[E] = ???
 
   def traverse[A, B](as: List[A])(f: A => F[B]): F[List[B]] =
     as.foldRight(unit(List[B]()))((a, acc) => map2(f(a), acc)(_ :: _))
