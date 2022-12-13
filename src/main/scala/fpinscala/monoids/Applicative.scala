eb62622caf3a1c3740bb306e5fbdb1b5cfc89f1c
Masashi Tsuru <0221masa@gmail.com>
2022-12-13 16:14:42 +0900

    Impl compose and map2

--- src/main/scala/fpinscala/monoids/Applicative.scala
+++ src/main/scala/fpinscala/monoids/Applicative.scala
@@ -42,7 +42,9 @@ trait Monad[F[_]] extends Applicative[F]:
   // Monadの実装では、少なくともunitを実装し、flatMapかjoinのいずれかとmapを上書きしなければならない
   def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B] = join(map(fa)(f))
   def join[A](ffa: F[F[A]]): F[A] = flatMap(ffa)(fa => fa)
-  def compose[A, B, C](f: A => F[B], g: B => F[C]): A => F[C]
+  def compose[A, B, C](f: A => F[B], g: B => F[C]): A => F[C] = a =>
+    flatMap(f(a))(b => g(b))
   override def map[A, B](fa: F[A])(f: A => B): F[B] =
     flatMap(fa)(a => unit(f(a)))
-  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C]
+  override def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
+    flatMap(fa)(a => map(fb)(b => f(a, b)))
