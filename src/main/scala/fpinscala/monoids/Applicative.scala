a3e2c4dd703929742c540142c7d6ca3141500485
Masashi Tsuru <0221masa@gmail.com>
2023-01-07 12:47:58 +0900

    Fix Traverse trait

--- src/main/scala/fpinscala/monoids/Applicative.scala
+++ src/main/scala/fpinscala/monoids/Applicative.scala
@@ -222,9 +222,9 @@ object Applicative {
   )
 }
 
-// TODO: map?
-// trait Traverse[F[_]]:
-//   def traverse[G[_]: Applicative, A, B](fa: F[A])(f: A => G[B]): G[F[B]] =
-//     sequence(map(fa)(f))
-//   def sequence[G[_]: Applicative, A](fga: F[G[A]]): G[F[A]] =
-//     traverse(fga)(ga => ga)
+trait Traverse[F[_]]:
+  def traverse[G[_]: Applicative, A, B](fa: F[A])(f: A => G[B]): G[F[B]] =
+    sequence(map(fa)(f))
+  def sequence[G[_]: Applicative, A](fga: F[G[A]]): G[F[A]] =
+    traverse(fga)(ga => ga)
+  def map[A, B](fa: F[A])(f: A => B): F[B] = ???
