5cdcb549e358d4742b02db56757d53aa2c0e8261
Masashi Tsuru <0221masa@gmail.com>
2023-02-04 09:56:18 +0900

    Refactoring?

--- src/main/scala/fpinscala/iomonad/Monad.scala
+++ src/main/scala/fpinscala/iomonad/Monad.scala
@@ -6,24 +6,27 @@ trait Functor[F[_]]:
 trait Monad[F[_]] extends Functor[F]:
   def unit[A](a: => A): F[A]
 
-  extension [A](fa: F[A])
-    def doWhile(cond: A => F[Boolean]): F[Unit] = for {
-      al <- fa
-      ok <- cond(al)
-      _ <- if (ok) doWhile(a)(cond) else unit(())
-    } yield ()
+  // extension [A](fa: F[A])
+  //   def doWhile(cond: A => F[Boolean]): F[Unit] = for {
+  //     al <- fa
+  //     ok <- cond(al)
+  //     _ <- if (ok) doWhile(a)(cond) else unit(())
+  //   } yield ()
 
-    def forever[B]: F[B] = {
-      lazy val t: F[B] = forever(fa)
-      fa flatMap (_ => t)
-    }
+  //   def forever[B]: F[B] = {
+  //     lazy val t: F[B] = forever(fa)
+  //     fa flatMap (_ => t)
+  //   }
 
-    def foldM[B](l: Stream[A])(z: B)(f: (B, A) => F[B]): F[B] = l match
-      case h #:: t => f(z, h) flatMap (z2 => foldM(t)(z2)(f))
-      case _       => unit(z)
+  def foldM[A, B](l: LazyList[A])(z: B)(f: (B, A) => F[B]): F[B] = l match
+    case h #:: t => f(z, h) flatMap (z2 => foldM(t)(z2)(f))
+    case _       => unit(z)
 
-    def foldM_[B](l: Stream[A])(z: B)(f: (B, A) => F[B]): F[Unit] =
-      skip { foldM(l)(z)(f) }
+  // https://github.com/fpinscala/fpinscala/blob/second-edition/src/main/scala/fpinscala/answers/iomonad/Monad.scala#L67
+  def foldM_[A, B](l: LazyList[A])(z: B)(f: (B, A) => F[B]): F[Unit] =
+    // void ってなんすか・・・
+    foldM(l)(z)(f).void
+// skip { foldM(l)(z)(f) }
 
-    def foreachM(l: Stream[A])(f: A => F[Unit]): F[Unit] =
-      foldM_(l)(())((u, a) => skip(f(a)))
+// def foreachM(l: Stream[A])(f: A => F[Unit]): F[Unit] =
+//   foldM_(l)(())((u, a) => skip(f(a)))
