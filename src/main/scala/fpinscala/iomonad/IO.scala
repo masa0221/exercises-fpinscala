1b10b8b97dff502943ad59d7ec4a9b5d8c9af890
Masashi Tsuru <0221masa@gmail.com>
2023-02-16 13:23:18 +0900

    Impl step and run method

--- src/main/scala/fpinscala/iomonad/IO.scala
+++ src/main/scala/fpinscala/iomonad/IO.scala
@@ -118,6 +118,19 @@ object IOSample3 {
     def flatMap[B](f: A => Async[B]): Async[B] = FlatMap(this, f)
     def map[B](f: A => B): Async[B] = flatMap(f andThen (Return(_)))
 
+    def step[A](async: Async[A]): Async[A] = async match
+      case FlatMap(FlatMap(x, f), g) => step(x flatMap (a => f(a) flatMap g))
+      case FlatMap(Return(x), f)     => step(f(x))
+      case _                         => async
+
+    def run[A](async: Async[A]): Par[A] = step(async) match
+      case Return(a)  => Par.unit(a)
+      case Suspend(r) => r
+      case FlatMap(x, f) =>
+        x match
+          case Suspned(r) => Par.flatMap(r)(a => run(f(a)))
+          case _          => sys.error("Impossible step eliminates these cases")
+
   case class Return[A](a: A) extends Async[A]
   case class Suspend[A](resume: Par[A]) extends Async[A]
   case class FlatMap[A, B](sub: Async[A], k: A => Async[B]) extends Async[B]
