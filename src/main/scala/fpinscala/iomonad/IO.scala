b1a965afb33ccaa4a51d746bc4e9e26a0108a75a
Masashi Tsuru <0221masa@gmail.com>
2023-02-25 10:58:29 +0900

    Define console trait

--- src/main/scala/fpinscala/iomonad/IO.scala
+++ src/main/scala/fpinscala/iomonad/IO.scala
@@ -161,4 +161,20 @@ object IOSample3 {
   //     x match
   //       case Suspend(r) => Par.flatMap(r)(a => run(f(a)))
   //       case _          => sys.error("Impossible step eliminates these cases")
+
+  sealed trait Console[A]:
+    def toPar: Par[A]
+    def toThunk: () => A
+
+  case object ReadLine extends Console[Option[String]]:
+    def toPar = Par.lazyUnit(run)
+    def toThunk = () => run
+
+    def run: Option[String] =
+      try Some(readLine())
+      catch { case e: Exception => None }
+
+  case class PrintLine(line: String) extends Console[Unit]:
+    def toPar = Par.lazyUnit(println(line))
+    def toThunk = () => println(line)
 }
