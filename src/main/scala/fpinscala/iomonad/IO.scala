7215513a2fdaf5e0732cfa728e8c3e9f1862b21c
Masashi Tsuru <0221masa@gmail.com>
2023-03-05 14:32:04 +0900

    Impl translate

--- src/main/scala/fpinscala/iomonad/IO.scala
+++ src/main/scala/fpinscala/iomonad/IO.scala
@@ -227,7 +227,17 @@ object IOSample3 {
   def runConsolePar[A](a: Free[Console, A]): Par[A] =
     runFree[Console, Par, A](a)(consoleToPar)
 
-  def translate[F[_], G[_], A](f: Free[F, A])(fg: F ~> G): Free[G, A] = ???
+  def translate[F[_], G[_], A](f: Free[F, A])(fg: F ~> G): Free[G, A] =
+    type FreeG[A] = Free[G, A]
+    val t = new (F ~> FreeG):
+      def apply[A](a: F[A]): Free[G, A] = Suspend { fg(a) }
 
-  def runConsole[A](a: Free[Console, A]): A = ???
+    runFree(f)(t)(freeMonad[G])
+
+  def runConsole[A](a: Free[Console, A]): A =
+    runTrampoline {
+      translate(a)(new (Console ~> Function0) {
+        def apply[A](c: Console[A]) = c.toThunk
+      })
+    }
 }
