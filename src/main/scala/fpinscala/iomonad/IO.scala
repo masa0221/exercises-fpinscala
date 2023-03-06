8b65a5042bf60591f3fa82e7c93d345259e71551
Masashi Tsuru <0221masa@gmail.com>
2023-03-06 13:33:34 +0900

    Impl consoleReader

--- src/main/scala/fpinscala/iomonad/IO.scala
+++ src/main/scala/fpinscala/iomonad/IO.scala
@@ -240,4 +240,18 @@ object IOSample3 {
         def apply[A](c: Console[A]) = c.toThunk
       })
     }
+
+  case class ConsoleReader[A](run: String => A) {
+    def map[B](f: A => B): ConsoleReader[B] =
+      ConsoleReader(r => f(run(r)))
+    def flatMap[B](f: A => ConsoleReader[B]): ConsoleReader[B] =
+      ConsoleReader(r => f(run(r)).run(r))
+  }
+
+  object ConsoleReader {
+    given Monad[ConsoleReader] with
+      def unit[A](a: => A) = ConsoleReader(_ => a)
+      def flatMap[A, B](ra: ConsoleReader[A])(f: A => ConsoleReader[B]) =
+        ra flatMap f
+  }
 }
