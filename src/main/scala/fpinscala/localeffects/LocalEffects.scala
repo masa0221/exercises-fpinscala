8118907191be1c2f210010523be29c048f41923e
Masashi Tsuru <0221masa@gmail.com>
2023-03-12 14:15:44 +0900

    Impl ST

--- src/main/scala/fpinscala/localeffects/LocalEffects.scala
+++ src/main/scala/fpinscala/localeffects/LocalEffects.scala
@@ -31,4 +31,27 @@ object LocalEffects {
     qs(0, arr.length - 1)
     arr.toList
   }
+
+  sealed trait ST[S, A] { self =>
+    protected def run(s: S): (A, S)
+    def map[B](f: A => B): ST[S, B] = new ST[S, B]:
+      def run(s: S) = {
+        val (a, s1) = self.run(s)
+        (f(a), s1)
+      }
+
+    def flatMap[B](f: A => ST[S, B]): ST[S, B] = new ST[S, B]:
+      def run(s: S) = {
+        val (a, s1) = self.run(s)
+        f(a).run(s1)
+      }
+  }
+
+  object ST {
+    def apply[S, A](a: => A) = {
+      lazy val memo = a
+      new ST[S, A]:
+        def run(s: S) = (memo, s)
+    }
+  }
 }
