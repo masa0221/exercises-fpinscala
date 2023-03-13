50c5493966e823f14cedd7943f4564e09d60c7c3
Masashi Tsuru <0221masa@gmail.com>
2023-03-13 13:26:32 +0900

    Impl STRef

--- src/main/scala/fpinscala/localeffects/LocalEffects.scala
+++ src/main/scala/fpinscala/localeffects/LocalEffects.scala
@@ -54,4 +54,20 @@ object LocalEffects {
         def run(s: S) = (memo, s)
     }
   }
+
+  sealed trait STRef[S, A] {
+    protected var cell: A
+    def read: ST[S, A] = ST(cell)
+    def write(a: A): ST[S, Unit] = new ST[S, Unit]:
+      def run(s: S) = {
+        cell = a
+        ((), s)
+      }
+  }
+
+  object STRef {
+    def apply[S, A](a: A): ST[S, STRef[S, A]] = ST(new STRef[S, A] {
+      var cell = a
+    })
+  }
 }
