c601a094a1e9757d5def3f970b9f2b6ca7ee4a0e
Masashi Tsuru <0221masa@gmail.com>
2023-03-15 13:27:49 +0900

    Add sample of how to use RunnableST

--- src/main/scala/fpinscala/localeffects/LocalEffects.scala
+++ src/main/scala/fpinscala/localeffects/LocalEffects.scala
@@ -53,6 +53,9 @@ object LocalEffects {
       new ST[S, A]:
         def run(s: S) = (memo, s)
     }
+
+    def runST[A](st: RunnableST[A]): A =
+      st.apply[Unit].run(())._1
   }
 
   sealed trait STRef[S, A] {
@@ -73,5 +76,18 @@ object LocalEffects {
 
   trait RunnableST[A] {
     def apply[S]: ST[S, A]
+  }
+
+  val p = new RunnableST[(Int, Int)] {
+    def apply[S] = for {
+      r1 <- STRef(1)
+      r2 <- STRef(2)
+      x <- r1.read
+      y <- r2.read
+      _ <- r1.write(y + 1)
+      _ <- r2.write(x + 1)
+      a <- r1.read
+      b <- r2.read
+    } yield (a, b)
   }
 }
