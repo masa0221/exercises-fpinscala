73392eb0883da19646b4855806a700d7a500a0e3
Masashi Tsuru <0221masa@gmail.com>
2023-03-16 13:29:18 +0900

    Impl STArray

--- src/main/scala/fpinscala/localeffects/LocalEffects.scala
+++ src/main/scala/fpinscala/localeffects/LocalEffects.scala
@@ -90,4 +90,24 @@ object LocalEffects {
       b <- r2.read
     } yield (a, b)
   }
+
+  sealed abstract class STArray[S, A](implicit manifest: Manifest[A]) {
+    protected def value: Array[A]
+    def size: ST[S, Int] = ST(value.size)
+    def write(i: Int, a: A): ST[S, Unit] = new ST[S, Unit] {
+      def run(s: S) = {
+        value(i) = a
+        ((), s)
+      }
+    }
+    def read(i: Int): ST[S, A] = ST(value(i))
+    def freeze: ST[S, List[A]] = ST(value.toList)
+  }
+
+  object STArray {
+    def apply[S, A: Manifest](sz: Int, v: A): ST[S, STArray[S, A]] =
+      ST(new STArray[S, A] {
+        lazy val value = Array.fill(sz)(v)
+      })
+  }
 }
