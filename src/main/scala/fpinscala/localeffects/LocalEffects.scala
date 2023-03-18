265206587cb33ea72d08b4a5c9c04452ab5c879c
Masashi Tsuru <0221masa@gmail.com>
2023-03-18 13:16:06 +0900

    Impl fill method

--- src/main/scala/fpinscala/localeffects/LocalEffects.scala
+++ src/main/scala/fpinscala/localeffects/LocalEffects.scala
@@ -103,7 +103,9 @@ object LocalEffects {
     def read(i: Int): ST[S, A] = ST(value(i))
     def freeze: ST[S, List[A]] = ST(value.toList)
 
-    def fill(xs: Map[Int, A]): ST[S, Unit] = ???
+    def fill(xs: Map[Int, A]): ST[S, Unit] = xs.foldRight(ST[S, Unit](())) {
+      case ((k, v), st) => st flatMap (_ => write(k, v))
+    }
   }
 
   object STArray {
