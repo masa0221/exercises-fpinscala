d5ae472e1e8a8206a66bf72c17d925b32bf40419
Masashi Tsuru <0221masa@gmail.com>
2023-04-07 13:34:35 +0900

    Add lines method in StreamingIO

--- src/main/scala/fpinscala/streamingio/StreamingIO.scala
+++ src/main/scala/fpinscala/streamingio/StreamingIO.scala
@@ -13,4 +13,9 @@ object StreamingIO {
       count > 4000
     } finally src.close
   }
+
+  def lines(filename: String): IO[Stream[String]] = IO {
+    val src = io.Source.fromFile(filename)
+    src.getLines.toStream append { src.close; Stream.empty }
+  }
 }
