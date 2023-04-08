a1f1d0a8f82ac5dcdb7557bc4647f32c7de5653d
Masashi Tsuru <0221masa@gmail.com>
2023-04-08 14:06:28 +0900

    Add Process class

--- src/main/scala/fpinscala/streamingio/StreamingIO.scala
+++ src/main/scala/fpinscala/streamingio/StreamingIO.scala
@@ -19,3 +19,16 @@ object StreamingIO {
     src.getLines.toStream append { src.close; Stream.empty }
   }
 }
+
+sealed trait Process[I, O]
+
+case class Emit[I, O](
+    head: O,
+    tail: Process[I, O] = Halt[I, O]()
+) extends Process[I, O]
+
+case class Await[I, O](
+    recv: Option[I] => Process[I, O]
+) extends Process[I, O]
+
+class Halt[I, O]() extends Process[I, O]
