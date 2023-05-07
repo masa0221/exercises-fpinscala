94dbce522247d6d4c1a8f5f62c4ff7eebde22d1e
Masashi Tsuru <0221masa@gmail.com>
2023-05-07 13:08:09 +0900

    Impl zip method

--- src/main/scala/fpinscala/streamingio/StreamingIO.scala
+++ src/main/scala/fpinscala/streamingio/StreamingIO.scala
@@ -110,6 +110,22 @@ object Process:
         case (o, s2) => emit(o, loop(s2)(f))
     )
 
+  def zip[A, B, C](p1: Process[A, B], p2: Process[A, C]): Process[A, (B, C)] =
+    (p1, p2) match
+      case (Halt(), _)                => Halt()
+      case (_, Halt())                => Halt()
+      case (Emit(b, t1), Emit(c, t2)) => Emit((b, c), zip(t1, t2))
+      case (Await(recv1), _) =>
+        Await((oa: Option[A]) => zip(recv1(oa), feed(oa)(p2)))
+      case (_, Await(recv2)) =>
+        Await((oa: Option[A]) => zip(feed(oa)(p1), recv2(oa)))
+
+  def feed[A, B](oa: Option[A])(p: Process[A, B]): Process[A, B] =
+    p match
+      case Halt()      => p
+      case Emit(h, t)  => Emit(h, feed(oa)(t))
+      case Await(recv) => recv(oa)
+
 sealed trait Process[I, O]:
   import Process.*
 
@@ -165,7 +181,7 @@ sealed trait Process[I, O]:
     }
 
   def zip[O2](p: Process[I, O2]): Process[I, (O, O2)] =
-    ???
+    Process.zip(this, p)
 
   def zipWithIndex: Process[I, (O, Int)] =
     this zip (count map (_ - 1))
