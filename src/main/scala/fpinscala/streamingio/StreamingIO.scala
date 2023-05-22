6714ade9925d0f32d7d46f56f43a113b2b899d5c
Masashi Tsuru <0221masa@gmail.com>
2023-05-22 13:43:34 +0900

    Impl Process object

--- src/main/scala/fpinscala/streamingio/StreamingIO.scala
+++ src/main/scala/fpinscala/streamingio/StreamingIO.scala
@@ -239,4 +239,15 @@ object SimpleStreamTransducers:
 object GeneralizedStreamTransducers:
   trait Process[F[_], O]
 
-  object Process
+  object Process:
+    case class Await[F[_], A, O](
+        req: F[A],
+        recv: Either[Throwable, A] => Process[F, O]
+    ) extends Process[F, O]
+
+    case class Emit[F[_], O](head: O, tail: Process[F, O]) extends Process[F, O]
+
+    case class Halt[F[_], O](err: Throwable) extends Process[F, O]
+
+    case object End extends Exception
+    case object Kill extends Exception
