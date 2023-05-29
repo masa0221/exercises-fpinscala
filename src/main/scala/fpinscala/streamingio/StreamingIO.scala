a69488f0528fadf8fb010a35f3f4ddac5c7df4ec
Masashi Tsuru <0221masa@gmail.com>
2023-05-29 13:23:37 +0900

    Impl await1 method

--- src/main/scala/fpinscala/streamingio/StreamingIO.scala
+++ src/main/scala/fpinscala/streamingio/StreamingIO.scala
@@ -295,7 +295,14 @@ object GeneralizedStreamTransducers:
     def await1[I, O](
         recv: I => Process1[I, O],
         fallback: => Process1[I, O] = halt1[I, O]
-    ): Process1[I, O] = ???
+    ): Process1[I, O] = Await(
+      Get[I],
+      (e: Either[Throwable, I]) =>
+        e match
+          case Left(End) => fallback
+          case Left(err) => Halt(err)
+          case Right(i)  => Try(recv(i))
+    )
 
     def emit1[I, O](h: O, tl: Process1[I, O] = halt1[I, O]): Process1[I, O] =
       emit(h, tl)
