e1998acd7629ac99600f452483baad648622220c
Masashi Tsuru <0221masa@gmail.com>
2023-06-09 17:35:45 +0900

    Impl Tee type

--- src/main/scala/fpinscala/streamingio/StreamingIO.scala
+++ src/main/scala/fpinscala/streamingio/StreamingIO.scala
@@ -423,3 +423,35 @@ object GeneralizedStreamTransducers:
         }
         lines
       } { src => eval_ { IO { src.close } } }
+
+    case class T[I, I2]() {
+      sealed trait f[X] { def get: Either[I => X, I2 => X] }
+      val L = new f[I] { def get = Left(identity) }
+      val R = new f[I2] { def get = Right(identity) }
+    }
+    def L[I, I2] = T[I, I2]().L
+    def R[I, I2] = T[I, I2]().R
+
+    type Tee[I, I2, O] = Process[T[I, I2]#f, O]
+
+    def haltT[I, I2, O]: Tee[I, I2, O] = Halt[T[I, I2]#f, O](End)
+    def awaitL[I, I2, O](
+        recv: I => Tee[I, I2, O],
+        fallback: => Tee[I, I2, O] = haltT[I, I2, O]
+    ): Tee[I, I2, O] = await[T[I, I2]#f, I, O](L) {
+      case Left(End) => fallback
+      case Left(err) => Halt(err)
+      case Right(a)  => Try(recv(a))
+    }
+    def awaitR[I, I2, O](
+        recv: I2 => Tee[I, I2, O],
+        fallback: => Tee[I, I2, O] = haltT[I, I2, O]
+    ): Tee[I, I2, O] = await[T[I, I2]#f, I2, O](R) {
+      case Left(End) => fallback
+      case Left(err) => Halt(err)
+      case Right(a)  => Try(recv(a))
+    }
+    def emitT[I, I2, O](
+        h: O,
+        tl: Tee[I, I2, O] = haltT[I, I2, O]
+    ): Tee[I, I2, O] = emit(h, tl)
