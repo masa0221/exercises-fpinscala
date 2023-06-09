21043381849e6b233d05b8a7e5d55961710396a3
Masashi Tsuru <0221masa@gmail.com>
2023-06-09 16:40:50 +0900

    Add line method

--- src/main/scala/fpinscala/streamingio/StreamingIO.scala
+++ src/main/scala/fpinscala/streamingio/StreamingIO.scala
@@ -390,3 +390,14 @@ object GeneralizedStreamTransducers:
 
     def eval_[F[_], A, B](a: F[A]): Process[F, B] =
       eval[F, A](a).drain[B]
+
+    def line(filename: String): Process[IO, String] =
+      resource { IO(io.Source.fromFile(filename)) } { src =>
+        lazy val iter = src.getLines
+        def step = if (iter.hasNext) Some(iter.next) else None
+        lazy val lines: Process[IO, String] = eval(IO(step)).flatMap {
+          case None       => Halt(End)
+          case Some(line) => Emit(line, lines)
+        }
+        lines
+      } { src => eval_ { IO { src.close } } }
